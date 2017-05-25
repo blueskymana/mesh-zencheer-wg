796940d3d7a850be35920a8cdb5217680796b4cc
blueskymana <1024791744@qq.com>
2017-05-25 17:48:55 +0800

    add management port.

---
+++ mesh/usr/bin/ebtables.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "The ebtables start !"
+
+ebtables -P FORWARD ACCEPT 
+ebtables -P INPUT ACCEPT
+ebtables -P OUTPUT ACCEPT
+ebtables -F 
+ebtables -A FORWARD -p ipv4 -i br-lan --ip-dst 172.16.16.16 -j DROP
